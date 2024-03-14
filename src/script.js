3d24b50ecd43cdacd7d6a9885622c76635ff46d1
aleoses22 <medranooses22@gmail.com>
2024-03-14 10:55:52 -0600

    condicionales, vercion1

---
+++ src/script.js
@@ -0,0 +1,54 @@
+var peras;
+var fresas;
+var mangos;
+var mensaje;
+
+
+function jugo_de_fresas() {
+  window.alert(mensaje);
+  document.queryselector('header').innerHTML = mensaje
+  
+}
+
+function servir_vaso_de_agua() {
+  window.alert(mensaje);
+  document.queryselector('header').innerHTML = mensaje
+
+}
+
+
+function jugo_de_peras() {
+  window.alert(mensaje);
+  document.queryselector('header').innerHTML = mensaje
+
+}
+
+function jugo_de_mangos() {
+  window.alert(mensaje);
+  document.querylselector('header').innerHTML = mensaje
+
+}
+
+
+fresas = true;
+peras = false;
+mangos = false;
+if (fresas) {
+  mensaje = "jugo de fresas"
+  jugo_de_fresas();
+} else if (peras) {
+  jugo_de_peras();
+  mensaje = "jugo de peras"
+} else if (mangos) {
+  mensaje = "jugo de mango"
+  jugo_de_mangos();
+} else {
+  mensaje = "sirveme un vaso de agua"
+  servir_vaso_de_agua();
+}
+
+
+
+
+
+
